6ccf8f8279f49e282af651efb3cd28256e36460d
hideaki.murakami <murakami@192.168.0.5>
2014-12-17 23:57:07 +0900

    generic add

--- Java1.5Test/src/jp/co/sample/GenericTest.java
+++ Java1.5Test/src/jp/co/sample/GenericTest.java
@@ -1,10 +1,61 @@
 package jp.co.sample;
 
+import java.util.ArrayList;
+
 public class GenericTest {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	static class Queue<T> {
+		final int SIZE = 5;
+		private ArrayList<T> values = new ArrayList<T>();
+		private int head = 0;
+		private int tail = 0;
 
+		Queue() {
+			for (int i=0; i<SIZE+1; i++) {
+				values.add(null);
+			}
+		}
+		
+		boolean enqueue(T data) {
+			if (data == null) return false;
+			if (((tail + 1) % values.size()) == head) {
+				return false;
+			}
+			values.set(tail++, data);
+			tail = tail % values.size();
+			return true;
+		}
+		
+		T dequeue() {
+			T data = null;
+			if (tail != head) {
+				data = values.get(head++);
+				head = head % values.size();
+			}
+			return data;
+		}
+		
+		boolean isEmpty() {
+			return (tail == head);
+		}
+	}
+	public static void main(String[] args) {
+		
+		Queue<Long> q = new Queue<Long>();
+		q.enqueue(1L);
+		q.enqueue(2L);
+		q.enqueue(3L);
+		q.enqueue(4L);
+		q.enqueue(5L);
+		q.enqueue(6L);
+		q.dequeue();
+		q.enqueue(7L);
+
+		while(!q.isEmpty()) {
+			long data = q.dequeue();
+			System.out.println(data + ",");
+		}
+		System.out.println("");
 	}
 
 }
